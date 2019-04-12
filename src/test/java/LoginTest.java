517431ef9fa1d12c088b1d33b075f8c2576b9664
Andrii Zubalii <andrii.zubalii@gmail.com>
2019-04-12 19:10:30 +0300

    Lesson #3

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -8,7 +10,23 @@ public class LoginTest {
     public void successfulLoginTest() {
         System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.linkedin.com");
+        String linkedinUrl = "https://www.linkedin.com";
+        String userEmail = "auto.test.email01@gmail.com";
+        String userPassword = "linked123";
+
+        driver.get(linkedinUrl);
+
+        WebElement emailField = driver.findElement(By.xpath("//input[@id='login-email']"));
+        WebElement passwordField = driver.findElement(By.xpath("//input[@id='login-password']"));
+        WebElement signinButton = driver.findElement(By.xpath("//input[@id='login-submit']"));
+
         Assert.assertEquals(driver.getTitle(), "LinkedIn: Log In or Sign Up ");
+        Assert.assertEquals(emailField.getAttribute("placeholder"),"Email");
+        Assert.assertEquals(passwordField.getAttribute("placeholder"),"Password");
+        Assert.assertEquals(signinButton.getAttribute("value"),"Sign in");
+        emailField.sendKeys(userEmail);
+        passwordField.sendKeys(userPassword);
+        signinButton.click();
+
     }
 }
